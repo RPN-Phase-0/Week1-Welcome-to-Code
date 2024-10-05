b6a6267b53c72512f87d7aa9815518cb533befea
alpharii <alphari404@gmail.com>
2024-10-05 12:18:56 +0700

    part 2 finish

---
+++ quiz/part2/index.js
@@ -0,0 +1,87 @@
+//1. Let's Form a Sentence
+
+// Problem
+// Pada tugas ini kamu diminta untuk melakukan penambahan string menggunakan simbol +.
+//  Disediakan variable word. Tambahkan nilai word satu per satu dengan nilai variable lain
+//  untuk membentuk sebuah kalimat. Jangan lupa menambahkan spasi di setiap kata, dan
+//   tampilkan di console hasil penggabungannya! Kamu tidak perlu membuat variable baru!
+
+let first = 'JavaScript';
+let second = 'is';
+let third = 'awesome';
+let fourth = 'and';
+let fifth = 'I';
+let sixth = 'love';
+let seventh = 'it!';
+
+console.log(first + second + third + fourth + fifth + sixth + seventh);
+
+// ## Soal 2. Index Accessing - 1 by 1
+
+// Problem
+// Pada tugas ini kamu diminta untuk "memecah" sebuah kalimat dan menampilkan setiap kata didalamnya.
+//  Untuk soal nomor ini, gunakan akses satu per satu karakter dari string untuk mengambil setiap huruf dalam kata.
+//   Terasa manual? Tidak apa-apa, kita coba ini dulu untuk saat ini.
+
+// Hints
+// Saat kamu mendapatkan tiap huruf, untuk membentuk setiap kata kamu tinggal menggunakan simbol + untuk membentuk kata
+//  tersebut!
+
+let word = 'wow JavaScript is so cool';
+let exampleFirstWord = word[0] + word[1] + word[2];
+let exampleSecondWord = word[4] + word[5] + word[6] + word[7] + word[8] + word[9] + word[10] + word[11] + word[12] + word[13];
+let exampleThirdWord = word[15] + word[16];
+let exampleFourthWord = word[18] + word[19]; 
+let exampleFifthWord = word[21] + word[22] + word[23] + word[24];
+
+
+console.log('First Word: ' + exampleFirstWord);
+console.log('Second Word:' + exampleSecondWord);
+console.log('Third Word: ' + exampleThirdWord);
+console.log('Fourth Word: ' + exampleFourthWord);
+console.log('Fifth Word: ' + exampleFifthWord);
+
+
+// ## Soal 3. Breaking Sentence (Again) using Substring
+
+// Problem
+// Mirip seperti soal nomor 2, namun kali ini gunakan substring untuk mengambil potongan dari tiap kata!
+
+let word3 = 'wow JavaScript is so cool';
+let exampleFirstWord3 = word3.substring(0, 3);
+let exampleSecondWord3 = word3.substring(4, 14);
+let exampleThirdWord3 = word3.substring(15, 17);
+let exampleFourthWord3 = word3.substring(18, 20);
+let exampleFifthWord3 = word3.substring(21, 25);
+
+console.log('First Word: ' + exampleFirstWord3);
+console.log('Second Word: ' + exampleSecondWord3);
+console.log('Third Word: ' + exampleThirdWord3);
+console.log('Fourth Word: ' + exampleFourthWord3);
+console.log('Fifth Word: ' + exampleFifthWord3);
+
+
+// ## Soal 4. Breaking Sentence (yet Again) and Count Each Length
+
+// Problem
+// Mirip seperti soal nomor 3, tapi tampilkan juga panjang kata masing-masingnya!
+
+
+let word4 = 'wow JavaScript is so cool';
+let exampleFirstWord4 = word4.substring(0, 3);
+let exampleSecondWord4 = word4.substring(4, 14);
+let exampleThirdWord4 = word4.substring(15, 17);
+let exampleFourthWord4 = word4.substring(18, 20);
+let exampleFifthWord4 = word4.substring(21, 25);
+
+let firstWordLength = exampleFirstWord4.length;
+let secondWordLength = exampleSecondWord4.length;
+let thirdWordLength = exampleThirdWord4.length;
+let fourthWordLength = exampleFourthWord4.length;
+let fifthWordLength = exampleFifthWord4.length;
+
+console.log('First Word: ' + exampleFirstWord4 + ', with length: ' + firstWordLength);
+console.log('Second Word: ' + exampleSecondWord4 + ', with length: ' + secondWordLength);
+console.log('Third Word: ' + exampleThirdWord4 + ', with length: ' + thirdWordLength);
+console.log('Fourth Word: ' + exampleFourthWord4 + ', with length: ' + fourthWordLength);
+console.log('Fifth Word: ' + exampleFifthWord4 + ', with length: ' + fifthWordLength);
