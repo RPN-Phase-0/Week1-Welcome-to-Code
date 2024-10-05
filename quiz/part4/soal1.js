52d0bffd2034cc1f2c4403bd0c282ca03754e713
alpharii <alphari404@gmail.com>
2024-10-05 13:45:16 +0700

    part 4 finish

---
+++ quiz/part4/soal1.js
@@ -0,0 +1,11 @@
+// Tugas 1
+// Buatlah sebuah fungsi bernama shoutOut(), yang mengembalikan nilai berupa "Halo Function!", 
+// yang kemudian akan ditampilkan di console.
+
+//bikin fungsinya disini
+
+function shoutOut() {
+    return "Halo Function!";
+}
+
+console.log(shoutOut());
---
+++ quiz/part4/soal3.js
@@ -0,0 +1,20 @@
+// Buatlah sebuah fungsi bernama processSentence(), yang akan memproses seluruh parameter yang diinput menjadi 
+// satu kalimat berikut: 
+// "Nama saya [Name], umur saya [Age] tahun, alamat saya di [Address], dan saya punya hobby yaitu [hobby]!"
+
+//bikin fungsinya disini
+
+let name = "Agus";
+let age = 30;
+let address = "Jln. Malioboro, Yogjakarta";
+let hobby = "gaming";
+
+
+function processSentence(name,age,address,hobby){
+    return `Nama saya ${name}, umur saya ${age} tahun, alamat saya di ${address}, dan saya punya hobby yaitu ${hobby}!`;
+}
+
+
+
+let fullSentence = processSentence(name,age,address,hobby);
+console.log(fullSentence); // Menampilkan "Nama saya Agus, umur saya 30 tahun, alamat saya di Jln. Malioboro, Yogjakarta, dan saya punya hobby yaitu gaming!"
