cce23e77c5cc91d04091eca39ac6d1e596c2a6bf
emangbolehsehansitu <136932079+emangbolehsehansitu@users.noreply.github.com>
2024-10-07 17:06:02 +0700

    Update tugaspart2.js

--- quiz/part2/tugaspart2.js
+++ quiz/part2/tugaspart2.js
@@ -18,3 +18,19 @@ let seventh = 'it!';
 
 word = word + " " + second + " " + third + " " + fourth + " " + fifth + " " + sixth + " " + seventh
 console.log(word)
+
+// Problem
+// Pada tugas ini kamu diminta untuk "memecah" sebuah kalimat dan menampilkan setiap kata didalamnya.
+//  Untuk soal nomor ini, gunakan akses satu per satu karakter dari string untuk mengambil setiap huruf dalam kata.
+//   Terasa manual? Tidak apa-apa, kita coba ini dulu untuk saat ini.
+
+// Hints
+// Saat kamu mendapatkan tiap huruf, untuk membentuk setiap kata kamu tinggal menggunakan simbol + untuk membentuk kata
+//  tersebut!
+
+let word = 'wow JavaScript is so cool';
+let exampleFirstWord = word[0] + word[1] + word[2] + word[3] + word[4] + word[5] + word[6] + word[7] + word[8]
++ word[9] + word[10] + word[11] + word[12] + word[13] + word[14] + word[15] + word[16] + word[17]
++ word[18] + word[19] + word[20] + word[21] + word[22] + word[23] + word[24];
+
+console.log('First Word: ' + exampleFirstWord);
