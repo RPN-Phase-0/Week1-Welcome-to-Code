6c4a946108910da06ed7edda5d90abe9c65217bb
emangbolehsehansitu <136932079+emangbolehsehansitu@users.noreply.github.com>
2024-10-07 18:40:33 +0700

    Update tugaspart3.js

--- quiz/part3/tugaspart3.js
+++ quiz/part3/tugaspart3.js
@@ -54,3 +54,27 @@ for (i = 0; i < 100; i++) {
         console.log(`${i} angkan ini ganjil`)
     }
 }
+
+// Buatlah 3 perulangan baru dari 1 - 100, dengan pertambahan counter sebesar 2, 5, dan 9.
+// Pada 3 perulangan baru ini periksa setiap angka counter:
+
+// Apabila bukan kelipatan yang ditentukan tidak perlu menuliskan apa-apa
+// Apabila angka counter adalah kelipatan 3 dengan pertambahan 2, kelipatan 6 dengan pertambahan 5, dan kelipatan 10 dengan pertambahan 9, tuliskan:
+// "3 kelipatan 3"dan seterusnya.
+
+// Buatlah 3 perulangan baru dari 1 - 100, dengan pertambahan counter sebesar 2, 5, dan 9.
+for ( let i = 0; i < 100; i = i + 2 ) {
+    if ( i % 3 == 0 ) {
+        console.log(`${i} kelipatan dari 3`)
+    }
+}
+for ( let i = 0; i < 100; i = i + 5 ) {
+    if ( i % 6 == 0 ) {
+        console.log(`${i} kelipatan dari 6`)
+    }
+}
+for ( let i = 0; i < 100; i = i + 9 ) {
+    if ( i % 10 == 0 ) {
+        console.log(`${i} kelipatan dari 10`)
+    }
+}
