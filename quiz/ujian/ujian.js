1b3bb827905ea43f635851cb12e9b87d14d3804c
emangbolehsehansitu <136932079+emangbolehsehansitu@users.noreply.github.com>
2024-10-07 19:59:53 +0700

    Update ujian.js

--- quiz/ujian/ujian.js
+++ quiz/ujian/ujian.js
@@ -19,4 +19,24 @@ function bandingkanAngka(angka1, angka2) {
     console.log(bandingkanAngka(4, 4)); // -1
     console.log(bandingkanAngka(3, 3)); // -1
     console.log(bandingkanAngka(17, 2)); // false
-  
+
+// Problem
+// Diberikan sebuah function balikKata(kata) yang menerima satu parameter berupa string. 
+// Function akan me-return kata yang dibalik. Contoh, jika kata adalah "John Doe", function akan me-return "eoD nhoJ".
+
+function balikKata(kata) {
+    let hasil = "";
+
+    for (let i = kata.length - 1; i >= 0; i--) {
+        hasil += kata[i];  
+    }
+
+    return hasil;
+}
+
+// TEST CASES
+console.log(balikKata('Hello World and Coders')); // sredoC dna dlroW olleH
+console.log(balikKata('John Doe')); // eoD nhoJ
+console.log(balikKata('I am a bookworm')); // mrowkoob a ma I
+console.log(balikKata('Coding is my hobby')); // ybboh ym si gnidoC
+console.log(balikKata('Super')); // repuS
