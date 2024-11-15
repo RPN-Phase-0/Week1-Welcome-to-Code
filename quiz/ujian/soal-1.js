ebc59ed70fbdf3a3e72a367cb5ff2ee31d9bc7eb
andikajayaw <andika@sindata.net>
2024-11-15 16:50:40 +0800

    ujian [soal-1] [soal-2]

---
+++ quiz/ujian/soal-1.js
@@ -0,0 +1,16 @@
+// Problem
+// Diberikan sebuah function bandingkanAngka(angka1, angka2) yang menerima dua parameter angka. 
+// Function akan me-return nilai true jika angka2 lebih besar dari angka1, dan false jika sebaliknya. 
+// Jika kedua angka bernilai sama, function akan me-return -1.
+
+function bandingkanAngka(angka1, angka2) {
+    //code disini
+    return angka2 > angka1 ? true : angka2 === angka1 ? -1 : false;
+}
+
+// TEST CASES
+console.log(bandingkanAngka(5, 8)); // true
+console.log(bandingkanAngka(5, 3)); // false
+console.log(bandingkanAngka(4, 4)); // -1
+console.log(bandingkanAngka(3, 3)); // -1
+console.log(bandingkanAngka(17, 2)); // false
