3b58175969a03ab31d5e9a73fcaab84ad8010d92
Reza Haffafi <mrezahaffafi@gmail.com>
2025-01-20 12:38:57 +0700

    Quiz2

--- quiz/part1/soal2.js
+++ quiz/part1/soal2.js
@@ -1,7 +1,45 @@
-for (let i = 0; i < 5; i++) {
-  for (let j = 0; j <= 5; j++) {
-    console.log(j);
-  }
-  console.log(" ");
+let hari = 21;
+let bulan = 1;
+let tahun = 1945;
+
+switch (bulan) {
+  case 1:
+    bulan = "januari";
+    break;
+  case 2:
+    bulan = "februari";
+    break;
+  case 3:
+    bulan = "maret";
+    break;
+  case 4:
+    bulan = "april";
+    break;
+  case 5:
+    bulan = "mei";
+    break;
+  case 6:
+    bulan = "juni";
+    break;
+  case 7:
+    bulan = "juli";
+    break;
+  case 8:
+    bulan = "agustus";
+    break;
+  case 9:
+    bulan = "september";
+    break;
+  case 10:
+    bulan = "oktober";
+    break;
+  case 11:
+    bulan = "november";
+    break;
+  case 12:
+    bulan = "desember";
+    break;
+  default:
+    console.log("ga ada bulannya kocak");
 }
-console.log("selesai");
+console.log(hari, bulan, tahun);
