70c529c0dd9b3c95a205d067e927964795629188
Joanita Timbin Panggalo <joanitatimbinp@gmail.com>
2025-01-20 09:55:24 +0700

    upload quiz w1 part3

---
+++ quiz/part3/quiz3.js
@@ -0,0 +1,16 @@
+
+// NOMOR 1
+console.log('=========== NO 1 ==============')
+let i = 0;
+
+while (i < 2) {
+    if (i === 0) {
+        console.log(`${i} adalah Pertama`);
+    } else if(i === 1){
+        console.log(`${i} adalah Kedua`);
+    } else{
+        console.log ('')
+    }
+    i++;
+}
+console.log('\n')
