dd7c21c2730edbe44683d1dffb81b31b1fa8c3eb
Reza Haffafi <mrezahaffafi@gmail.com>
2025-01-20 13:05:24 +0700

    Done

---
+++ quiz/part2/soal2.js
@@ -0,0 +1,21 @@
+let word = "wow JavaScript is so cool";
+let a = word[0] + word[1] + word[2];
+let b =
+  word[4] +
+  word[5] +
+  word[6] +
+  word[7] +
+  word[8] +
+  word[9] +
+  word[10] +
+  word[11] +
+  word[12] +
+  word[13];
+let c = word[15] + word[16];
+let d = word[18] + word[19];
+let e = word[21] + word[22] + word[23] + word[24];
+console.log("first word :", a);
+console.log("second word :", b);
+console.log("third word :", c);
+console.log("fourth word :", d);
+console.log("fifth word :", e);
---
+++ quiz/part2/soal4.js
@@ -0,0 +1,18 @@
+let word = "wow JavaScript is so cool";
+let a = word.substring(0, 3);
+let b = word.substring(4, 14);
+let c = word.substring(15, 17);
+let d = word.substring(18, 20);
+let e = word.substring(21, 25);
+
+let A = a.length;
+let B = b.length;
+let C = c.length;
+let D = d.length;
+let E = e.length;
+
+console.log("First Word :", a, "With Length :", A);
+console.log("Secons Word :", b, "With Length :", B);
+console.log("Third Word :", c, "With Length :", C);
+console.log("fourth Word :", d, "With Length :", D);
+console.log("fifth Word :", e, "With Length :", E);
