86b33b17214fdeb53e5decacd496cf318b523e42
Ravfael <ravfaelnovitohandoyo419@gmail.com>
2025-02-10 22:28:39 +0700

    latihan tambahan no2 sir

--- quiz/Latihan/latihan.js
+++ quiz/Latihan/latihan.js
@@ -12,3 +12,20 @@ for (let i = 1; i <= 100; i++) {
     console.log(i);
   }
 }
+
+// Hitung jumlah bilangan prima dari 1 hingga 100.
+
+let primeCount = 0;
+for (let i = 2; i <= 100; i++) {
+  let isPrime = true;
+  for (let j = 2; j < i; j++) {
+    if (i % j === 0) {
+      isPrime = false;
+      break;
+    }
+  }
+  if (isPrime) {
+    primeCount++;
+  }
+}
+console.log(`the total prime number from 1 to 100 is ${primeCount}`);
