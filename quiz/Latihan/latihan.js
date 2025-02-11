5e4c24032fd6fa6936fc8446cecadecdd9064070
Ravfael <ravfaelnovitohandoyo419@gmail.com>
2025-02-11 09:53:36 +0700

    all latihan tambahan (optional) done sir

--- quiz/Latihan/latihan.js
+++ quiz/Latihan/latihan.js
@@ -1,3 +1,5 @@
+// LATIHAN TAMBAHAN (OPTIONAL) YANG ADA DI PHASE 0 WEEK 1 PART 5
+
 //Modifikasi program untuk menampilkan bukan bilangan prima dari 1 hingga 100.
 
 for (let i = 1; i <= 100; i++) {
@@ -29,3 +31,25 @@ for (let i = 2; i <= 100; i++) {
   }
 }
 console.log(`the total prime number from 1 to 100 is ${primeCount}`);
+
+// Buat program untuk mengecek apakah sebuah angka yang diinput pengguna adalah bilangan prima.
+
+let inputUser = 11;
+let isPrime = true;
+
+if (inputUser <= 1) {
+  isPrime = false;
+} else {
+  for (let i = 2; i < Math.sqrt(inputUser); i++) {
+    if (inputUser % i === 0) {
+      isPrime = false;
+      break;
+    }
+  }
+}
+
+if (isPrime) {
+  console.log(`${inputUser} adalah bilangan prima`);
+} else {
+  console.log(`${inputUser} bukan bilangan prima`);
+}
