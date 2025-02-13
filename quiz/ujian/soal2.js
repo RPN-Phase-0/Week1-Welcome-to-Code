a608ec2227e4fe103b3781b1fb61909e7626eb74
MartinoKelvin <martinokelvin06032005@gmail.com>
2025-02-13 20:33:06 +0700

    Ujian Soal 2

---
+++ quiz/ujian/soal2.js
@@ -0,0 +1,21 @@
+// Problem
+// Diberikan sebuah function balikKata(kata) yang menerima satu parameter berupa string. 
+// Function akan me-return kata yang dibalik. Contoh, jika kata adalah "John Doe", function akan me-return "eoD nhoJ".
+
+function balikKata(kata) {
+    let banyakKata = kata.length;
+    let hasil = "";
+    for (let i = banyakKata - 1; i >= 0; i--) {
+
+        hasil += kata[i];
+    }
+
+    return hasil;
+}
+
+// TEST CASES
+console.log(balikKata('Hello World and Coders')); // sredoC dna dlroW olleH
+console.log(balikKata('John Doe')); // eoD nhoJ
+console.log(balikKata('I am a bookworm')); // mrowkoob a ma I
+console.log(balikKata('Coding is my hobby')); // ybboh ym si gnidoC
+console.log(balikKata('Super')); // repuS
