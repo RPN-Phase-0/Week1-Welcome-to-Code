00b2fc8c542756bf39cfb25285e812a277d0460b
Virts <193348169+vmmtss@users.noreply.github.com>
2025-02-13 20:43:30 +0700

    part 4 week 1

---
+++ quiz/part4/soal2.js
@@ -0,0 +1,10 @@
+function calculateMultiply(num1, num2) {
+    return num1 * num2
+}
+
+var num1 = 1
+var num2 = 2
+
+var hasilPerkalian = calculateMultiply(num1,num2);
+
+console.log(hasilPerkalian);
---
+++ quiz/part4/soal3.js
@@ -0,0 +1,11 @@
+function processSentence(name, age, address, hobby) {
+    return `Nama saya ${name}, umur saya ${age} tahun, alamat saya di ${address}, dan saya punya hobby yaitu ${hobby}`
+}
+
+var name = "Agus";
+var age = 30;
+var address = "Jln. Malioboro, Yogjakarta";
+var hobby = "gaming";
+
+var fullSentence = processSentence(name,age,address,hobby);
+console.log(fullSentence); // Menampilkan "Nama saya Agus, umur saya 30 tahun, alamat saya di Jln. Malioboro, Yogjakarta, dan saya punya hobby yaitu gaming!"
