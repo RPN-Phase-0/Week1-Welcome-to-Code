55f33a9742eb4b4109a61131842eff750dd58fa6
Jk <164493888+jksybill@users.noreply.github.com>
2025-02-19 21:43:32 +0700

    Create soal3.js

---
+++ quiz/part2/soal3.js
@@ -0,0 +1,12 @@
+let word = 'wow JavaScript is so cool';
+let word1 = word.substring(0, 3);
+let word2 = word.substring(4, 14);
+let word3 = word.substring(14, 17);
+let word4 = word.substring(18, 20);
+let word5 = word.substring(21, 25);
+
+console.log('First Word: ' + word1);
+console.log('Second Word: ' + word2);
+console.log('Third Word: ' + word3);
+console.log('Four Word: ' + word4);
+console.log('Fifth Word: ' + word5);
