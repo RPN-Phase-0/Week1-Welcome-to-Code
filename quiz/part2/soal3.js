dedfc8df4ed534b4dd7401ac811662948fd9d10a
Probe <110994698+xProbe@users.noreply.github.com>
2025-02-19 10:51:22 +0700

    submit -> part 1 & 2

---
+++ quiz/part2/soal3.js
@@ -0,0 +1,19 @@
+//Soal 3. Breaking Sentence (Again) using Substring
+
+// Problem
+// Mirip seperti soal nomor 2, namun kali ini gunakan substring untuk mengambil potongan dari tiap kata!
+
+let words = 'wow JavaScript is so cool';
+let firstWord = words.substring(0, 3);
+let secondWord = words.substring(4, 14);
+let thirdWord = words.substring(15, 17);
+let fourthWord = words.substring(18, 20);
+let fifthWord = words.substring(21, 25);
+
+
+
+console.log('First Word : ' + firstWord);
+console.log('Second Word : ' + secondWord);
+console.log('Third Word : ' + thirdWord);
+console.log('Fourth Word : ' + fourthWord);
+console.log('Fifth Word : ' + fifthWord);
