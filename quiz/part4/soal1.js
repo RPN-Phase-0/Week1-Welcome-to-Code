821825c8d1165a33ee58b5d80a7160a64d8c8afb
Probe <110994698+xProbe@users.noreply.github.com>
2025-02-19 12:01:14 +0700

    submit part 4

---
+++ quiz/part4/soal1.js
@@ -0,0 +1,16 @@
+// Soal 1
+
+// Tugas 1
+// Buatlah sebuah fungsi bernama shoutOut(), yang mengembalikan nilai berupa "Halo Function!", 
+// yang kemudian akan ditampilkan di console.
+
+//bikin fungsinya disini
+
+function shoutOut() {
+    return "Hello Function!"
+}
+
+
+// ------------------------------------
+console.log(shoutOut());
+// ------------------------------------
