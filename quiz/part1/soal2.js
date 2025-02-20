114197826dcc7135e36e4284ab290d5b8509a7e6
afhiya <faridrakapratama@gmail.com>
2025-02-21 06:40:05 +0700

    Quiz part 1

---
+++ quiz/part1/soal2.js
@@ -0,0 +1,44 @@
+let hari = 32,
+  bulan = 14,
+  tahun = 1900;
+
+switch (bulan) {
+  case 1:
+    console.log(`${hari} Januari ${tahun} `);
+    break;
+  case 2:
+    console.log(`${hari} Februari ${tahun} `);
+    break;
+  case 3:
+    console.log(`${hari} Maret ${tahun} `);
+    break;
+  case 4:
+    console.log(`${hari} April ${tahun} `);
+    break;
+  case 5:
+    console.log(`${hari} Mei ${tahun} `);
+    break;
+  case 6:
+    console.log(`${hari} Juni ${tahun} `);
+    break;
+  case 7:
+    console.log(`${hari} Juli ${tahun} `);
+    break;
+  case 8:
+    console.log(`${hari} Agustus ${tahun} `);
+    break;
+  case 9:
+    console.log(`${hari} September ${tahun} `);
+    break;
+  case 10:
+    console.log(`${hari} Oktober ${tahun} `);
+    break;
+  case 11:
+    console.log(`${hari} November ${tahun} `);
+    break;
+  case 12:
+    console.log(`${hari} Desember ${tahun} `);
+    break
+  default:
+    console.log("Masukkan range bulan 1-12")
+}
