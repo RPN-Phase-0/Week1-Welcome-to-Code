9caf2ed3a30cb72065a994b5d1504fd93f040cb3
ulum <apumm12@gmail.com>
2025-02-20 19:39:32 +0700

    menjawab soal2 part2

---
+++ quiz/part2/soal2.js
@@ -0,0 +1,8 @@
+let word = "wow JavaScript is so cool";
+let words = word.split(" "); // Memisahkan kata berdasarkan spasi menggunakan sting method split()
+
+console.log("First Word: " + words[0]);
+console.log("Second Word: " + words[1]);
+console.log("Third Word: " + words[2]);
+console.log("Fourth Word: " + words[3]);
+console.log("Fifth Word: " + words[4]);
