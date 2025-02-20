6f0f85ef7d3266b8002e90440583219fffd04f93
ulum <apumm12@gmail.com>
2025-02-20 20:29:34 +0700

    menjawab soal1-3 part4

---
+++ quiz/part4/soal2.js
@@ -0,0 +1,7 @@
+let num1 = 1;
+let num2 = 2;
+
+const calculateMultiply = () => num1 * num2;
+
+let hasilPerkalin = calculateMultiply();
+console.log(hasilPerkalin);
---
+++ quiz/part4/soal3.js
@@ -0,0 +1,10 @@
+let name = "Agus";
+let age = 30;
+let address = "Jln. Malioboro, Yogjakarta";
+let hobby = "gaming";
+
+const processSentence = () =>
+  `Nama saya ${name}, umur saya ${age} tahun, alamat saya ${address}, dan saya punya hobby yaitu ${hobby}`;
+
+let fullSentence = processSentence();
+console.log(fullSentence);
