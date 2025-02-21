db64c4b17c4d30c752b1ee6103b886704249f0b2
kzitoo <evanoktavianus007@gmail.com>
2025-02-21 17:03:27 +0700

    soal 2 part 2

---
+++ quiz/part2/soal2.js
@@ -0,0 +1,17 @@
+// Problem
+// Pada tugas ini kamu diminta untuk "memecah" sebuah kalimat dan menampilkan setiap kata didalamnya.
+//  Untuk soal nomor ini, gunakan akses satu per satu karakter dari string untuk mengambil setiap huruf dalam kata.
+//   Terasa manual? Tidak apa-apa, kita coba ini dulu untuk saat ini.
+
+// Hints
+// Saat kamu mendapatkan tiap huruf, untuk membentuk setiap kata kamu tinggal menggunakan simbol + untuk membentuk kata
+//  tersebut!
+
+let word = "wow JavaScript is so cool";
+let completeWord = "";
+
+for (let i = 0; i < word.length; i++) {
+  completeWord += word[i];
+}
+
+console.log("The Word Id : " + completeWord);
