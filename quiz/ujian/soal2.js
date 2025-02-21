4f2e971bcbb198201acfb75bafc977e8de75c86a
Eunbimz <timeforbima123@gmail.com>
2025-02-22 00:10:47 +0700

    soal1.js

---
+++ quiz/ujian/soal2.js
@@ -0,0 +1,20 @@
+// Problem
+// Diberikan sebuah function balikKata(kata) yang menerima satu parameter berupa string. 
+// Function akan me-return kata yang dibalik. Contoh, jika kata adalah "John Doe", function akan me-return "eoD nhoJ".
+
+function balikKata(kata) {
+    // you can only write your code here!
+    let reversedWord = "";
+    for (let i = kata.length - 1; i >= 0; i--) {
+        reversedWord += kata[i];
+    }
+    return reversedWord;
+
+}
+  
+// TEST CASES
+console.log(balikKata('Hello World and Coders')); // sredoC dna dlroW olleH
+console.log(balikKata('John Doe')); // eoD nhoJ
+console.log(balikKata('I am a bookworm')); // mrowkoob a ma I
+console.log(balikKata('Coding is my hobby')); // ybboh ym si gnidoC
+console.log(balikKata('Super')); // repuS
