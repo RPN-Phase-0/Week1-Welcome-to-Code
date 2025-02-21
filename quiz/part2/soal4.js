430ffab3e1a807dbdbd24431dc22f15096bd05bd
strgar <satriategarputu@gmail.com>
2025-02-21 12:56:44 +0700

    Submit quiz part 2 #soal4

---
+++ quiz/part2/soal4.js
@@ -0,0 +1,22 @@
+// Problem
+// Mirip seperti soal nomor 3, tapi tampilkan juga panjang kata masing-masingnya!
+
+
+let word4 = 'wow JavaScript is so cool';
+let firstWord4 = word4.substring(0, 3);
+let secondWord4 = word4.substring(4, 14);
+let thirdWord4 = word4.substring(15, 17);
+let fourthWord4 = word4.substring(18, 20);
+let fifthWord4 = word4.substring(21, 25);
+
+let firstWordLength = firstWord4.length;
+let secondWordLength = secondWord4.length;
+let thirdWordLength = thirdWord4.length;
+let fourthWordLength = fourthWord4.length;
+let fifthWordLength = fifthWord4.length;
+
+console.log('First Word: ' + firstWord4 + ', with length: ' + firstWordLength);
+console.log('Second Word: ' + secondWord4 + ', with length: ' + secondWordLength);
+console.log('Thidrd Word: ' + thirdWord4 + ', with length: ' + thirdWordLength);
+console.log('Fourth Word: ' + fourthWord4 + ', with length: ' + fourthWordLength);
+console.log('Fifth Word: ' + fifthWord4 + ', with length: ' + fifthWordLength);
