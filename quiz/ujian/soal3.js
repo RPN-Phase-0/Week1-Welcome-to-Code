8eb80d5280b99153a41b8488b840506354f9c79f
strgar <satriategarputu@gmail.com>
2025-02-21 19:11:40 +0700

    Submit ujian #soal 3

---
+++ quiz/ujian/soal3.js
@@ -0,0 +1,24 @@
+// Problem
+// Diberikan sebuah function konversiMenit(menit) yang menerima satu parameter berupa angka yang merupakan ukuran waktu 
+// dalam menit. Function akan me-return string waktu dalam format jam:menit berdasarkan menit tersebut. 
+// Contoh, jika menit adalah 63, maka function akan me-return "1:03".
+
+function konversiMenit(menit) {
+    // you can only write your code here!
+    let jam, sisaMenit;
+
+    jam = Math.floor(menit / 60);
+    sisaMenit = menit % 60;
+
+    if (sisaMenit < 10) {
+        sisaMenit = `0` + sisaMenit;
+    }
+    return jam + ':' + sisaMenit;
+}
+  
+// TEST CASES
+console.log(konversiMenit(63)); // 1:03
+console.log(konversiMenit(124)); // 2:04
+console.log(konversiMenit(53)); // 0:53
+console.log(konversiMenit(88)); // 1:28
+console.log(konversiMenit(120)); // 2:00
