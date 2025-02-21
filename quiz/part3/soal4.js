6731a9b4c942eeca1154eb747f5d5e01e2a906ac
Jk <164493888+jksybill@users.noreply.github.com>
2025-02-21 21:45:57 +0700

    Create soal4.js

---
+++ quiz/part3/soal4.js
@@ -0,0 +1,18 @@
+for (let i = 1; i <= 100; i += 2){
+    if (i % 3 === 0){
+        console.log(`${i} keilipatan 3`);
+        }
+}
+
+for (let i = 1; i <= 100; i += 5){
+    if (i % 6 === 0){
+        console.log(`${i} keilipatan 6`);
+        }
+}
+
+
+for (let i = 1; i <= 100; i += 9){
+    if (i % 10 === 0){
+        console.log(`${i} keilipatan 10`);
+        }
+}
