d5132b94bc1d7f96c7860149353ac3bc04789238
Jk <164493888+jksybill@users.noreply.github.com>
2025-02-21 21:44:51 +0700

    Create soal2.js

---
+++ quiz/part3/soal2.js
@@ -0,0 +1,11 @@
+console.log("Looping Pertama");
+
+for (let i = 0; i <= 9; i++){
+    console.log('maju' + i);
+};
+
+console.log("Looping kedua");
+
+for (let i = 9; i >= 0; i--){
+    console.log('mundur' + i);
+};
