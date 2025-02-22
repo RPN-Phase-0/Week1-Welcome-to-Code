56cd2378e5bb155ca8838ac10f4af7abb41e98c5
Muhammad Farouq <muhammadfarouq225@gmail.com>
2025-02-22 11:42:26 +0700

    menyelesaikan tugas week1 part2

---
+++ quiz/part2/soal3.js
@@ -0,0 +1,12 @@
+let word3 = "wow JavaScript is so cool";
+let exampleFirstWord3 = word3.substring(0, 3);
+let secondWord = word3.substring(4, 14);
+let thirdWord = word3.substring(15, 17);
+let fourthWord = word3.substring(18, 20);
+let fifthWord = word3.substring(21, 25);
+
+console.log("First Word: " + exampleFirstWord3);
+console.log("Second Word: " + secondWord);
+console.log("Third Word: " + thirdWord);
+console.log("Fourth Word: " + fourthWord);
+console.log("Fifth Word: " + fifthWord);
