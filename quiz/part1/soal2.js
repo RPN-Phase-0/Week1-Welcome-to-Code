de0b99368faf5c85c14af824312551bd2834a2a1
Light510 <attaclp@gmail.com>
2025-02-22 08:33:28 +0700

    soal2.js

---
+++ quiz/part1/soal2.js
@@ -0,0 +1,44 @@
+//soal 2
+
+const prompt = require("prompt-sync")();
+let tanggal = prompt(`Masukkan tanggal: `);
+let bulan = prompt(`Masukkan Bulan: `);
+let tahun = prompt(`Masukkan Tahun: `);
+switch(bulan){
+    case "1":
+        console.log(`${tanggal} Januari ${tahun}`);
+        break;
+    case "2":
+        console.log(`${tanggal} Februari ${tahun}`);
+        break;
+    case "3":
+        console.log(`${tanggal} Maret ${tahun}`);
+        break;
+    case "4":
+        console.log(`${tanggal} April ${tahun}`);
+        break;
+    case "5":
+        console.log(`${tanggal} Mei ${tahun}`);
+        break;
+    case "6":
+        console.log(`${tanggal} Juni ${tahun}`);
+        break;
+    case "7":
+        console.log(`${tanggal} Juli ${tahun}`);
+        break;
+    case "8":
+        console.log(`${tanggal} Agustus ${tahun}`);
+        break;
+    case "9":
+        console.log(`${tanggal} September ${tahun}`);
+        break;
+    case "10":
+        console.log(`${tanggal} Oktober ${tahun}`);
+        break;
+    case "11":
+        console.log(`${tanggal} November ${tahun}`);
+        break;
+    case "12":
+        console.log(`${tanggal} Desember ${tahun}`);
+        break;
+}
