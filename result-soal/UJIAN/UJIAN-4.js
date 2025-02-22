adcb6f9a9ebea7c55aa31fe188278d41e1e132e3
ZiroiGT <168020365+ZiroiGT@users.noreply.github.com>
2025-02-23 02:16:38 +0700

    Create UJIAN-4.js

---
+++ result-soal/UJIAN/UJIAN-4.js
@@ -0,0 +1,14 @@
+//   Problem
+//   Diberikan sebuah function xo(str) yang menerima satu parameter berupa string. 
+//   Function akan me-return true jika jumlah karakter x sama dengan jumlah karakter o, dan false jika tidak.
+
+function xo(str) {
+    return (str.match(/x/gi) || []).length === (str.match(/o/gi) || []).length;
+}
+  
+// TEST CASES
+console.log(xo('xoxoxo')); // true
+console.log(xo('oxooxo')); // false
+console.log(xo('oxo')); // false
+console.log(xo('xxxooo')); // true
+console.log(xo('xoxooxxo')); // true
