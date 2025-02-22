889c475bee822ab9b7e4d5137ddc46604d1930ac
ZiroiGT <168020365+ZiroiGT@users.noreply.github.com>
2025-02-23 01:45:33 +0700

    Create SOAL-11.js

---
+++ result-soal/PART-3/SOAL-11.js
@@ -0,0 +1,18 @@
+// Problem buatlah bintang seperti berikut
+let input = 5
+//hasilnya
+//*
+//**
+//***
+//****
+//*****
+
+
+
+for (let i = 1; i <= input; i++) {
+    let star = '';
+    for (let j = 1; j <= i; j++) {
+        star += '*';
+    }
+    console.log(star);
+}
