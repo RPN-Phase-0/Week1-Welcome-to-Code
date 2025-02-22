7364b4ae8987f7f15f077b0f44cb4a74c77e40dd
harilesmana <harilesmana878@gmail.com>
2025-02-22 19:04:59 +0700

    soal2.js

---
+++ quiz/part2/soal2.js
@@ -0,0 +1,11 @@
+let saya_ganteng = 'JavaScript';
+let second = 'is';
+let third = 'awesome';
+let fourth = 'and';
+let fifth = 'I';
+let sixth = 'love';
+let seventh = 'it!';
+
+saya_ganteng = saya_ganteng + ' ' + second + ' ' + third + ' ' + fourth + ' ' + fifth + ' ' + sixth + ' ' + seventh;
+
+console.log(saya_ganteng);
