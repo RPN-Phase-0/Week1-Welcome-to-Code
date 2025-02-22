90670ceb5adbfe3c714857c4fbaff51b2e0a22ab
ZiroiGT <168020365+ZiroiGT@users.noreply.github.com>
2025-02-23 02:01:05 +0700

    Create SOAL-2.js

---
+++ result-soal/PART-4/SOAL-2.js
@@ -0,0 +1,19 @@
+// Buatlah sebuah fungsi bernama calculateMultiply(), yang mengembalikan nilai berupa hasil kali dari 
+// dua parameter yang dikirim.
+
+//bikin fungsinya disini
+
+function calculateMultiply(num1, num2) {
+    return num1 * num2;
+}
+
+
+let num1 = 953535353535335
+let num2 = 2345355345345353
+
+let hasilPerkalian = calculateMultiply(num1,num2);
+console.log(hasilPerkalian);
+
+// ------------------------------------
+console.log('')
+// -----------------------------------
