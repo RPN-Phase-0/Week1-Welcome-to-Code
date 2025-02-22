952c9acbb865e11508be3a3135e919c4a8e8ac87
ZiroiGT <168020365+ZiroiGT@users.noreply.github.com>
2025-02-23 00:03:26 +0700

    Create soal2.js

---
+++ result-soal/soal2.js
@@ -0,0 +1,44 @@
+
+
+// Soal 2
+
+// Kamu akan diberikan sebuah tanggal dalam tiga variabel, yaitu hari, bulan, dan tahun. Disini kamu diminta untuk membuat format tanggal. Misal tanggal yang diberikan adalah hari 1, bulan 5, dan tahun 1945. Maka, output yang harus kamu proses adalah menjadi 1 Mei 1945.
+
+// Gunakan switch case untuk kasus ini!
+
+// Contoh:
+
+// let hari = 21; let bulan = 1; let tahun = 1945;
+
+// Maka hasil yang akan tampil di console adalah: '21 Januari 1945';
+
+//tips gunakan keyword ini di google "conditional switch case javascript"
+//dan mulailah membaca dan mencontek di stackoverflow xixixi
+
+let tanggal = 12; // assign nilai variabel tanggal disini! (dengan angka antara 1 - 31)
+let bulan = 2; // assign nilai variabel bulan disini! (dengan angka antara 1 - 12)
+let tahun = 2001; // assign nilai variabel tahun disini! (dengan angka antara 1900 - 2200)
+
+const listbulan = [
+    "January", 
+    "February",
+    "March",   
+    "April",   
+    "May",     
+    "June",    
+    "July",    
+    "August",  
+    "September",
+    "October", 
+    "November", 
+    "December",
+];
+
+
+if (bulan >= 0 && bulan <= 11) {
+    console.log(tanggal +" " + listbulan[bulan - 1] + " " + tahun);
+} else {
+    console.log("Bulan tidak valid");
+}
+
+//code switch case kamu disini
