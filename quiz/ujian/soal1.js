a1553996b8d732b2cd713326a16e172aed95aaf6
Jk <164493888+jksybill@users.noreply.github.com>
2025-02-22 22:02:14 +0700

    Create soal1.js

---
+++ quiz/ujian/soal1.js
@@ -0,0 +1,16 @@
+function bandingkanAngka(angka1, angka2){
+    if (angka2 > angka1){
+        return true;
+    } else if (angka2 < angka1){
+        return false;
+    } else {
+        return -1;
+    }
+}
+    
+    // TEST CASES
+    console.log(bandingkanAngka(5, 8)); // true
+    console.log(bandingkanAngka(5, 3)); // false
+    console.log(bandingkanAngka(4, 4)); // -1
+    console.log(bandingkanAngka(3, 3)); // -1
+    console.log(bandingkanAngka(17, 2)); // false
