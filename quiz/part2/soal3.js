c3c40e2046def7f54634d7d9b7ec0349f4f2258c
Damm-kwon <adamlukman0602@gmail.com>
2025-02-23 20:52:49 +0700

    soal3fix

--- quiz/part2/soal3.js
+++ quiz/part2/soal3.js
@@ -1,4 +1,6 @@
 let word3 = 'wow JavaScript is so cool';
-let exampleFirstWord3 = word3.substring(0, 25);
+let exampleFirstWord3 = word3.substring(0, 3);
+let secondword3 = word3.substring(4, 13);
 
 console.log('First Word: ' + exampleFirstWord3);
+console.log('secondword3: ' + secondword3);
