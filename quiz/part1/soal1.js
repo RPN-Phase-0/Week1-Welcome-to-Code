00c91abbd519e5a5e8ed4105576e8ada9cf3add5
Damm-kwon <adamlukman0602@gmail.com>
2025-02-23 12:20:02 +0700

    soal2.js

--- quiz/part1/soal1.js
+++ quiz/part1/soal1.js
@@ -18,36 +18,15 @@
 // algoritma
 //isi algoritma mu disini (AWAS KALO GA DI ISI!!!!)
 
-let nama = "ijo", peran = "penyihir";
-if 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//soal 2
-let tanggal = "6";
-let bulan = "mei";
-let tahun = "2008";
-
-
-//soal selesai sir
+let nama = "ijo", peran = "ksatria";
+if (peran === "ksatria") 
+if (nama === "") {
+if (peran ==="")
+    console.log("halo Ksatria ${nama} , kamu dapat menyerang dengan senjatamu");
+} else if (peran === tabib) {
+    console.log("halo Tabib ${nama} , kamu akan membantu temanmu yang terluka");
+}else if  (peran === "penyihir") {
+    console.log("halo Penyihir ${nama} , ciptakan keajaiban yang membantu kemenanganmu!");
+}else  {
+    console.log("tapi kayaknya kamu jadi bot aja ya, peran yang kamu pilih ga ada");
+};
