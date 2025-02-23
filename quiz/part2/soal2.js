3f5e53a7ff557fbcaed42ef1f496fe370d40f58e
Damm-kwon <adamlukman0602@gmail.com>
2025-02-23 20:46:45 +0700

    soal2fix

--- quiz/part2/soal2.js
+++ quiz/part2/soal2.js
@@ -1,4 +1,5 @@
 let word = 'wow JavaScript is so cool';
-let exampleFirstWord = word[0] + word[1] + word[2] + word[3] + word[4] + word[5] + word[6] + word[7] + word[8] + word[9] + word[10] + word[11] + word[12] + word[13] + word[14] + word[15] + word[16] + word[17] + word[18] + word[19] + word[20] + word[21] + word[22] + word[23] + word[24];
-
+let exampleFirstWord = word[0] + word[1] + word[2];
+let secondword = word[3] + word[4] + word[5] + word[6] + word[7] + word[8] + word[9] + word[10] + word[11] + word[12] + word[13];
 console.log('First Word: ' + exampleFirstWord);
+console.log('second Word:' + secondword);
