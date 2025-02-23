f516a2a8ec2751050a5b6e721c54ef29b4ccd9f6
0wnerz <0wnerz.dev@gmail.com>
2025-02-23 16:24:07 +0700

    0wnerz weak1 part2 soal 1-4

---
+++ quiz/part2/soal4.js
@@ -0,0 +1,14 @@
+let word4 = 'wow JavaScript is so cool';
+let Firstword = word4.substring(0, 3);
+let Middleword = word4.substring(4, 14)
+let Endword = word4.substring(15, 17);
+let Lastword = word4.substring(18, 20);
+let Finalword = word4.substring(21, 25);
+//length
+let Firstwordlength = Firstword.length;
+let Middlewordlength = Middleword.length;
+let Endwordlength = Endword.length;
+let Lastwordlength = Lastword.length;
+let Finalwordlength = Finalword.length;
+
+console.log ('word:' + Firstword + ' ' + 'with length:' + Firstwordlength + ' ' + ',word:' + Middleword + ' ' + 'with length:' + Middlewordlength + ' ' + ',word:' + Endword + ' ' + ',with length:' + Endwordlength + ' ' + ',word:' + Lastword + ' ' + 'with length:' + Lastwordlength + ' ' + ',word:' + Finalword + ' ' + 'with length:' + Finalwordlength);
