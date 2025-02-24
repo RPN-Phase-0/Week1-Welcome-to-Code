bf61cc094c1e6b20092cb9a512f708bf2d7b9f56
Damm-kwon <adamlukman0602@gmail.com>
2025-02-24 18:41:29 +0700

    ketidakpuasan,soal1

---
+++ quiz/part3/soal1.js
@@ -0,0 +1,14 @@
+let i = 0;
+let j = 10;
+
+console.log("looping maju");
+while (i <= 10) {
+console.log("looping maju " + i);
+    i++;
+}
+
+console.log("looping mundur");
+while (j >= 0) {
+    console.log("looping mundur " +j);
+    j--;
+}
---
+++ quiz/part3/test1.js
@@ -0,0 +1,15 @@
+let j = 10
+let i = 0
+let text = "looping maju " 
+let text1 = "looping mundur "
+while (i < 10) {
+  text += "looping maju " + i;
+  i++;
+  console.log("looping maju " + i)
+}
+
+while (j > 0) {
+  text += "looping mundur " - j;
+  j--;
+  console.log("looping mundur " - j)
+}
